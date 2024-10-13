32ce70aec70b7ed9bb75085442f34e323f803422
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-10-13 20:30:58 +0700

    last updated

--- bai103/bai103b.py
+++ bai103/bai103b.py
@@ -8,6 +8,46 @@ def generate_pascals_triangle(n):
         triangle.append(row)
     return triangle
 def print_triangle(h):
-    triangle = generate_pascals_triangle(h)
-    print(triangle)
+    triangle = generate_pascals_triangle(h)[::-1]
+    for i in range(1, 2*h):
+        if i <= h: 
+            j = 0       
+            c = i // 2        
+            try: 
+                while j < i: 
+                    if (i%2 == 0 and j%2 == 0) or (i%2 == 1 and j%2 == 1):
+                        print("   ", end="")
+                        if j == 0: 
+                            c -= 1
+                        j += 1 
+                    else: 
+                        print(triangle[j][c], end=" ")
+                        j += 1
+                        c -= 1
+            except IndexError: 
+                print("  ", end="")
+            for k in range(h-i): 
+                print("  ", end="")
+            print("")
+        else: 
+            j=0
+            c=i//2
+            try: 
+
+                while j < (2*h - i): 
+                    if (i%2 == 0 and j%2 == 0) or (i%2 == 1 and j%2 == 1):
+                        print("   ", end="")
+                        if j == 0: 
+                            c -= 1
+                        j+=1
+                    else: 
+                        print(triangle[j][c], end=" ")
+                        c-=1
+                        j+=1
+            except IndexError: 
+                print("  ", end="")
+            for k in range(i-h):
+                print("  ", end="")
+            print("")
+            
 print_triangle(5)
